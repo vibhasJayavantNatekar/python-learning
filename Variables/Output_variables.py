ed2302b35f664e18cbb2643200256924ad9c26a6
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-12-12 12:45:23 +0530

    Learn about variables

---
+++ Variables/Output_variables.py
@@ -0,0 +1,14 @@
+#Output Variables
+
+x = "Python  is awesome"
+print(x)
+
+#Output multiple variables
+
+x = "Python "
+y = "is"
+z = "awesome"
+print(x + y + z)
+
+#Using '+' operator to output multiple variables
+
