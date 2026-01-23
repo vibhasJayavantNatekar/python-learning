0078a4524a12608df600444a3b74bda898068bbe
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2026-01-23 11:59:00 +0530

    Started learning about OOP in python

---
+++ OOP/Dele_object.py
@@ -0,0 +1,14 @@
+#Create a Class
+
+class MyClass:
+    x = 5
+
+#Createa a Object
+p1 = MyClass()
+
+print(p1.x)
+
+#Delete object
+
+
+del p1
---
+++ OOP/create_object.py
@@ -0,0 +1,21 @@
+#Create a Class
+
+class MyClass:
+    x = 5
+
+#Createa a Object
+p1 = MyClass()
+print(p1.x)
+
+
+#Multiple Objects
+
+p2 = MyClass()
+p3 = MyClass()
+p4 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+print(p4.x)
+
