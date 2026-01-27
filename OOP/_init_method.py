2c00e438b38b043a52b46e18df2a348b5344ddca
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2026-01-27 09:40:05 +0530

    Learn more about OOP

--- OOP/_init_method.py
+++ OOP/_init_method.py
@@ -9,3 +9,17 @@ p1 = Person("Emil",36)
 print(p1.name)
 print(p1.age)
 
+#Default value in __init__()
+
+class Person2:
+    def __init__(self , name , age = 18):
+        self.name = name
+        self.age = age
+
+n1 = Person2("Vibhas")
+n2 = Person2("sam", 12)
+
+print(n1.name , n1.age)
+print(n2.name , n2.age)
+
+
